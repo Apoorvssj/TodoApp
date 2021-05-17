1be67bfd38670cbb417adf9adf7ee23a03b06e00
Apoorv Yadav <apoorv2yadav@gmail.com>
2021-05-17 15:00:12 +0530

    added github svg link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,7 @@ import TodoList from "../components/TodoList";
 import Lottie from "lottie-react-web";
 import suchEmpty from "../animations/suchEmpty.json";
 import Wave from "../components/Wave";
+import githubLeft from "../img/github-corner-left.svg";
 
 export const UserContext = React.createContext();
 
@@ -30,6 +31,9 @@ const Home = () => {
   return (
     <UserContext.Provider value={{ tasks, setTasks, status, setStatus }}>
       <StyledHome>
+        <Github href="https://github.com/Apoorvssj/TodoApp">
+          <img src={githubLeft} alt="Fork me on GitHub" />
+        </Github>
         <Header>
           <motion.h1 variants={colorAnimation} initial="hidden" animate="show">
             Dump your To-Do's here
@@ -71,11 +75,18 @@ const StyledHome = styled(motion.div)`
 const Header = styled(motion.div)`
   display: flex;
   justify-content: center;
-  align-items: center;
+  /* align-items: center; */
+  align-items: flex-end;
   min-height: 20vh;
   h1 {
+    /* margin-top: 1rem; */
     color: #23d997;
     font-size: 2rem;
+  }
+  @media only screen and (max-width: 640px) {
+    h1 {
+      font-size: 1.5rem;
+    }
   }
 `;
 
@@ -87,6 +98,13 @@ const Empty = styled(motion.div)`
   justify-content: end;
   align-items: center;
   z-index: 10;
+`;
+
+const Github = styled.a`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 20;
 `;
 
 export default Home;
